792f783a801e853602020bde0da01c0d97afca76
neila-a <neilaspace@outlook.com>
2022-11-14 22:18:05 +0800

    来自Makefile的一次自动提交

---
+++ Makefile
@@ -0,0 +1,13 @@
+.PHONY: dev build start lint push
+dev:
+	npx next dev
+build:
+	npx next build
+start:
+	npx next start
+lint:
+	npx next lint
+push: 
+	git add .
+	git commit -a -m "来自Makefile的一次自动提交"
+	git push origin main
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Head from "next/head";
+function Index(): JSX.Element {
+    return (<>
+        <Head>
+            <title>翻转</title>
+        </Head>
+    </>);
+};
+export default Index;
