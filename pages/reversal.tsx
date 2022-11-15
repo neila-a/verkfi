9b47cd04868a56a23e1fae57eea7174134040c17
neila-a <neilaspace@outlook.com>
2022-11-15 22:18:49 +0800

    来自Makefile的一次自动提交

--- pages/reversal.tsx
+++ pages/reversal.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import Head from "next/head";
 import {
-    Typography
+    Typography,
+    TextField,
+    Alert
 } from "@mui/material";
 import {} from "@mui/icons-material";
 function Index(): JSX.Element {
@@ -10,6 +12,8 @@ function Index(): JSX.Element {
             <link rel="shortcut icon" href="/favicon.ico" />
             <title>翻转</title>
         </Head>
+        <Alert severity="info">以空格分隔。</Alert>
+        <TextField fullWidth label="输入框" id="input" />
     </>);
 };
 export default Index;
