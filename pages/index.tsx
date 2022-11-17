af1dbea79844a6559bb1967dc365947fb50b087b
neila-a <neilaspace@outlook.com>
2022-11-17 22:28:43 +0800

    来自Makefile的一次自动提交

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,62 @@
-import Router from "next/router";
+import Head from "next/head";
 import HeadBar from "../components/HeadBar";
+import * as React from 'react';
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText
+} from "@mui/material";
 function Index(): JSX.Element {
+    const pages = [
+        {
+            name: "AudioTools",
+            to: "audiotools"
+        },
+        {
+            name: "CountLetter",
+            to: "countletter"
+        },
+        {
+            name: "BigTime",
+            to: "bigtime"
+        },
+        {
+            name: "π计算器",
+            to: "pi"
+        },
+        {
+            name: "翻转",
+            to: "reversal"
+        },
+        {
+            name: "掷骰子",
+            to: "shaizi"
+        },
+    ];
     return (
         <>
+            <Head>
+                <title>NeilaNotes</title>
+            </Head>
             <HeadBar isIndex={true} pageName="NeilaNotes" />
+            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                <nav aria-label="main mailbox folders">
+                    <List>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemText primary="Inbox" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemText primary="Drafts" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </nav>
+            </Box>
         </>
     );
 }
