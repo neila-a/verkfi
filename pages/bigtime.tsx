e76eb308a4e0ce83b72a4fc938ae793dcba0349d
neila-a <neilaspace@outlook.com>
2022-12-02 18:59:10 +0800

    来自Makefile的一次自动提交

--- pages/bigtime.tsx
+++ pages/bigtime.tsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HeadBar from "../components/HeadBar";
 import moment from "moment";
 function BigTime(): JSX.Element {
     var [hours, setHours] = useState<number>();
     var [minutes, setMinutes] = useState<number>();
     var [seconds, setSeconds] = useState<number>();
-    const getTime = function () {
-        setHours(moment().hour());
-        setMinutes(moment().minute());
-        setSeconds(moment().second());
-    };
-    getTime();
-    setTimeout(getTime, 1000);
+    useEffect(function () {
+        const getTime = function () {
+            setHours(moment().hour());
+            setMinutes(moment().minute());
+            setSeconds(moment().second());
+        };
+        getTime();
+        setInterval(getTime, 1000);
+    }, [moment, setHours, setMinutes, setSeconds]);
     return (
         <>
             <HeadBar isIndex={false} pageName="BigTime" />
