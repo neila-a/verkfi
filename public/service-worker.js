0e53d7c09a78337f791cea04ec79d82e53d8f805
neila-a <neilaspace@outlook.com>
2022-12-21 18:22:55 +0800

    来自Makefile的一次自动提交

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const
     version = '1.1.0',
     dev = true,
-    devVersion = 12
+    devVersion = 13
     CACHE = `NeilaTools-${version}-${dev ? `dev-${devVersion}` : "prod"}`,
     installFilesEssential = [
         '/',
