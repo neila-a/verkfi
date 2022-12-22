1d4883dd0978b282f444a1c93a0bd6174e5cdb28
neila-a <neilaspace@outlook.com>
2022-12-22 15:04:36 +0800

    Filter: 向外暴露image

--- pages/filter.tsx
+++ pages/filter.tsx
@@ -10,6 +10,12 @@ import {
     pages
 } from "./index";
 import imageStyle from "../styles/Filter.module.scss";
+declare global {
+    interface Window {
+        image: any[];
+        setImage: React.Dispatch<React.SetStateAction<any[]>>
+    }
+};
 export const ImageTypes: ImageType[] = [
     "blur",
     "brightness",
@@ -28,34 +34,35 @@ export function Image(props: {
     src: string;
 }): JSX.Element {
     return (
-        <img title={props.type} key={props.type} className={imageStyle[props.type]} src={/* Test Delete props.src */"/libear-only.png"} alt={props.type}></img>
+        <img title={props.type} key={props.type} className={imageStyle[props.type]} src={props.src} alt={props.type}></img>
     );
 };
 export default function Filter(): JSX.Element {
     var [image, setImage] = useState<any[]>([]);
+    
     useEffect(function () {
-    });
+        window.image = image;
+        window.setImage = setImage;
+    }, [image, setImage]);
     return (
         <>
             <HeadBar isIndex={false} pageName={pages[6].name} />
-            <div id="images">
-                {ImageTypes.map(function (type) {
-                    return (
-                        <div key={type}>
-                            <Image type={type} src={image[0]} />
-                        </div>
-                    );
-                })}
-            </div>
             <FilePond
                 files={image}
                 onupdatefiles={setImage}
                 allowMultiple={true}
                 maxFiles={1}
                 server="/api"
-                name="files" /* sets the file input name, it's filepond by default */
+                name="files"
                 labelIdle='拖拽文件到这里、粘贴或<span class="filepond--label-action">浏览</span>'
             />
+            <div id="images">
+                {ImageTypes.map((type) => (
+                    <div key={type}>
+                        <Image type={type} src={/* Test Deleted image[0] */"/libear-only.png"} />
+                    </div>
+                ))}
+            </div>
         </>
     );
 };
