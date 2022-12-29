d8f885ea7f76b462d618e3f7ed84627e66c173a3
neila-a <neilaspace@outlook.com>
2022-12-29 14:48:47 +0800

    :bug: 没有删除import "fork-me-on-github"

--- components/HeadBar.tsx
+++ components/HeadBar.tsx
@@ -10,7 +10,6 @@ import {
 } from "@mui/icons-material"
 import Router from "next/router";
 import Head from "next/head";
-import ForkMeOnGitHub from "fork-me-on-github";
 import {
 	useEffect,
 	useState
