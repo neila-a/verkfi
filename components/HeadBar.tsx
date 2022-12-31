e1670885a005e9c2fe443ebfcbe638168d0bcf4d
neila-a <neilaspace@outlook.com>
2022-12-31 14:05:46 +0800

    :loud_sound: && :heavy_plus_sign: eruda

--- components/HeadBar.tsx
+++ components/HeadBar.tsx
@@ -16,6 +16,7 @@ import {
 } from "react";
 import style from "../styles/HeadBar.module.scss";
 import LpLogger from "lp-logger";
+import eruda from "eruda";
 export var logger = new LpLogger({
     name: "HeadBar",
     level: "log", // 空字符串时，不显示任何信息
@@ -31,18 +32,36 @@ export interface HeadBarOption {
  */
 export default function HeadBar(props: HeadBarOption): JSX.Element {
 	var [forkMeOnGitHub, setForkMeOnGitHub] = useState<boolean>(true);
+	var [erudaEnabled, setErudaEnabled] = useState<boolean>(false);
 	useEffect(function () {
 		switch (window.localStorage.getItem("fork-me-on-github")) {
 			case "true":
-				setForkMeOnGitHub(true);
+				logger.log("检测到\"Fork me on GitHub\"为开启状态。");
 				break;
 			case "false":
 				setForkMeOnGitHub(false);
+				logger.log("检测到\"Fork me on GitHub\"为关闭状态。");
 				break;
 			default:
 				window.localStorage.setItem("fork-me-on-github", "true");
+				logger.log("检测到\"Fork me on GitHub\"为空，已设置为开启状态。");
+				break;
 		}
-	}, [setForkMeOnGitHub]);
+		switch (window.localStorage.getItem("eruda-enabled")) {
+			case "true":
+				setErudaEnabled(true);
+				eruda.init();
+				logger.log("检测到Eruda为启用状态，已初始化。");
+				break;
+			case "false":
+				logger.log("检测到Eruda为关闭状态。");
+				break;
+			default:
+				window.localStorage.setItem("eruda-enabled", "false");
+				logger.log("检测到Eruda为空，已设置为关闭状态。");
+				break;
+		};
+	}, [setForkMeOnGitHub, setErudaEnabled]);
 	return (
 		<>
 			<Head>
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -16,28 +16,51 @@ export var logger = new LpLogger({
 import * as React from "react";
 export default function Settings(): JSX.Element {
     var [forkMeOnGitHub, setForkMeOnGitHub] = useState<boolean>(true);
+	var [erudaEnabled, setErudaEnabled] = useState<boolean>(false);
 	useEffect(function () {
 		switch (window.localStorage.getItem("fork-me-on-github")) {
 			case "true":
-				setForkMeOnGitHub(true);
+				logger.log("检测到\"Fork me on GitHub\"为开启状态。");
 				break;
 			case "false":
 				setForkMeOnGitHub(false);
+				logger.log("检测到\"Fork me on GitHub\"为关闭状态。");
 				break;
 			default:
 				window.localStorage.setItem("fork-me-on-github", "true");
+				logger.log("检测到\"Fork me on GitHub\"为空，已设置为开启状态。");
+				break;
 		}
-	}, [setForkMeOnGitHub]);
+		switch (window.localStorage.getItem("eruda-enabled")) {
+			case "true":
+				setErudaEnabled(true);
+				logger.log("检测到Eruda为启用状态。");
+				break;
+			case "false":
+				logger.log("检测到Eruda为关闭状态。");
+				break;
+			default:
+				window.localStorage.setItem("eruda-enabled", "false");
+				logger.log("检测到Eruda为空，已设置为关闭状态。");
+				break;
+		};
+	}, [setForkMeOnGitHub, setErudaEnabled]);
     return (
         <>
             <HeadBar isIndex={false} pageName="设置" />
             <FormGroup>
-                <FormControlLabel control={<Switch checked={forkMeOnGitHub} onChange={(event) => {
+                <FormControlLabel control={<Switch checked={forkMeOnGitHub} onChange={event => {
                     setForkMeOnGitHub(event.target.checked);
                     window.localStorage.setItem("fork-me-on-github", String(event.target.checked));
                     logger.log(`已设置选项"Fork Me On GitHub" 为 ${forkMeOnGitHub}。`);
                     window.location.reload();
                 }}/>} label="Fork Me On GitHub" />
+                <FormControlLabel control={<Switch checked={erudaEnabled} onChange={event => {
+                    setErudaEnabled(event.target.checked);
+                    window.localStorage.setItem("eruda-enabled", String(event.target.checked));
+                    logger.log(`已设置选项"开发控制台" 为 ${erudaEnabled}。`);
+                    window.location.reload();
+                }}/>} label="开发控制台" />
             </FormGroup>
         </>
     );
