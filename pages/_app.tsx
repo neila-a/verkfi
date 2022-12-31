2318898cc68b8a9db92cb7b8d5b0f16fa2a5970d
neila-a <neilaspace@outlook.com>
2022-12-31 19:24:18 +0800

    移动ServiceWorker位置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,9 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
         if ('serviceWorker' in navigator) {
             // register service worker
-            window.UWAWorker = navigator.serviceWorker.register("/js/service-worker.js", {
-                scope: "https://tools.neila.ga/"
-            });
+            window.UWAWorker = navigator.serviceWorker.register("/service-worker.js");
             window.UWAWorker.then((registration) => logger.log(`Service worker for UWA register success: %o`, registration)).catch((reason) => logger.error(`Service worker for UWA register fail: ${reason}`));
         }
     }, []);
