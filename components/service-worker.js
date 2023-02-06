11c2d8c529fb5267dedf7c1b4417bb6842accff2
neila-a <neilaspace@outlook.com>
2023-02-06 21:01:26 +0800

    :bug: 改为/tools/

--- components/service-worker.js
+++ components/service-worker.js
@@ -15,16 +15,16 @@ const
     Cache = `NeilaTools-${version}-${devVersion != 0 ? `dev${devVersion}` : "prod"}`, // C
     installFilesEssential = [
         '/',
-        ...toolsTo.map(addr => `/pages/${addr}`),
+        ...toolsTo.map(addr => `/tools/${addr}`),
         '/index.webmanifest',
         '/favicon.png'
     ];
-const installStaticFiles = () => caches.open(Cache).then(cache => cache.addAll(installFilesEssential)); // install static assets
+const installStaticFiles = () => caches.open(Cache).then(cache => cache.addAll(installFilesEssential).catch(reason => console.error(reason))); // install static assets
 const clearOldCaches = () => caches.keys().then(keylist => Promise.all(keylist.filter(key => key !== Cache).map(key => caches.delete(key))));
 self.addEventListener('install', event => event.waitUntil(installStaticFiles().then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(clearOldCaches().then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET') return;
+    if (event.request.method !== 'GET') return;reason
     let { url } = event.request;
     event.respondWith(
         caches.open(Cache).then(cache => {
