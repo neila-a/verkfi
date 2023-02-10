f8825f7c103183fc5389dc19c1f8ee90365076ca
neila-a <neilaspace@outlook.com>
2023-02-10 21:33:40 +0800

    :sparkles:
    Signed-off-by: neila-a <neilaspace@outlook.com>

--- tools/mathgen.tsx
+++ tools/mathgen.tsx
@@ -2,9 +2,12 @@ import {
     Checkbox,
     FormControlLabel,
     FormGroup,
-    TextField
+    TextField,
+    Button,
+    Typography
 } from "@mui/material";
 import {
+    Fragment,
     useEffect,
     useState
 } from "react";
@@ -14,6 +17,7 @@ import {
 import {
     destroyer
 } from "./reversal";
+import style from "../styles/MathGen.module.scss";
 import LpLogger from "lp-logger";
 export var logger = new LpLogger({
     name: "MathGen",
@@ -27,54 +31,115 @@ export const defaultCalcs: calc[] = [
     "÷"
 ];
 export default function MathGen(): JSX.Element {
-    var [calcs, setCalcs] = useState<calc[]>(defaultCalcs);
+    var [calcs, setCalcs] = useState<calc[]>([
+        "+",
+        "-"
+    ]),
+        [min, setMin] = useState<number>(0),
+        [max, setMax] = useState<number>(10),
+        [itemCount, setItemCount] = useState<number>(20),
+        [maths, setMath] = useState<string[]>([]),
+        [showOut, setShowOut] = useState<boolean>(false);
     useEffect(function () {
         logger.log("calcs为", calcs);
-    }, [calcs])
+    }, [calcs]);
+    function calcMath() {
+        var calcMaths: string[] = [];
+        calcs.forEach(mode => {
+            for (var up = "", step = 1; step < (itemCount / (calcs.length)); step++) {
+                var one = Math.floor(Math.random() * (max - min) + min);
+                var two: number;
+                switch (mode) {
+                    case "-":
+                        while (two <= one) {
+                            two = Math.floor(Math.random() * (max - min) + min);
+                        }
+                        break;
+                    default:
+                        two = Math.floor(Math.random() * (max - min) + min);
+                        break;
+                }
+                calcMaths.push(`${one}${mode}${two}=${eval(`${one}${mode.replace("×", "*").replace("÷", "/")}${two}`)}`);
+            }
+        });
+        setMath(calcMaths);
+    };
+    useEffect(calcMath, [
+        min,
+        max,
+        itemCount
+    ]);
     return (
         <div>
             <br />
             <FormGroup>
                 <TextField label="上限" type="number" InputLabelProps={{
                     shrink: true,
+                }} onChange={event => {
+                    setMax(Number(event.currentTarget.value));
                 }} />
                 <br />
                 <TextField label="下限" type="number" InputLabelProps={{
                     shrink: true,
+                }} onChange={event => {
+                    setMin(Number(event.currentTarget.value));
                 }} />
                 <br />
                 <TextField label="个数" type="number" InputLabelProps={{
                     shrink: true,
+                }} onChange={event => {
+                    setItemCount(Number(event.currentTarget.value));
                 }} />
-                <>
+                <Fragment>
                     <FormControlLabel label="全选" control={
                         <Checkbox checked={calcs != emptyArray} indeterminate={calcs != defaultCalcs} onChange={event => {
                             switch (event.currentTarget.checked) {
-                                case true:
+                                case false:
                                     setCalcs([]);
                                     break;
-                                case false:
+                                case true:
                                     setCalcs(defaultCalcs);
                                     break;
                             }
                         }} />
                     } />
-                    <>
+                    <Fragment>
                         {defaultCalcs.map(calc => <FormControlLabel label={calc} key={calc} control={
                             <Checkbox checked={calcs.includes(calc)} onChange={event => {
                                 switch (event.currentTarget.checked) {
-                                    case true:
+                                    case false:
                                         setCalcs(destroyer(calcs, calc));
                                         break;
-                                    case false:
+                                    case true:
                                         setCalcs([...calcs, calc]);
                                         break;
                                 }
                             }} />
                         } />)}
-                    </>
-                </>
+                    </Fragment>
+                </Fragment>
+                <Button variant="contained" onClick={calcMath}>计算</Button>
+                <Button variant="outlined" onClick={event => {
+                    showOut ? setShowOut(false) : setShowOut(true);
+                }}>{showOut ? "隐藏" : "显示"}答案</Button>
             </FormGroup>
+            {maths == emptyArray ? <Fragment /> : maths.map(math => {
+                function ThisComponent(): JSX.Element {
+                    var [isError, setError] = useState<boolean>(false);
+                    return (
+                        <div key={math} className={style["single"]}>
+                            <Typography>{math.replace(/=.*/g, "")}</Typography>
+                            <TextField label="个数" type="number" InputLabelProps={{
+                                shrink: true,
+                            }} error={isError} onChange={event => {
+                                setError((event.currentTarget.value == math.replace(/.*=/g, "")) ? false : true);
+                            }} />
+                            {showOut ? <Typography>答案：{math.replace(/.*=/g, "")}</Typography> : <Fragment />}
+                        </div>
+                    );
+                }
+                return <ThisComponent key={math} />;
+            })}
         </div>
     );
 }
