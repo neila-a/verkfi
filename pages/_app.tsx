9604e842abf591b41c1091707fe70157d04a188f
Neila <neila@neila.ga>
2023-03-11 10:00:26 +0800

    :ambulance:手机端没有registerProtocolHandler()

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,9 @@ import {
 import LpLogger from "lp-logger";
 import style from "../styles/ModifiedApp.module.scss";
 import "../styles/App.scss";
+import {
+    isMobile
+} from 'react-device-detect';
 export var logger = new LpLogger({
     name: "NeilaTools",
     level: "log", // 空字符串时，不显示任何信息
@@ -23,8 +26,13 @@ declare global {  //设置全局属性
 export default function ModifiedApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
         var url = `${location.origin}/tool?handle=%s`;
+        if (!isMobile) {
+            logger.log("检测到此设备并非手机");
+            navigator.registerProtocolHandler("neilatools", url);
+        } else {
+            logger.log("检测到此设备为手机，停止注册协议");
+        }
         if ('serviceWorker' in navigator) {
-            navigator.registerProtocolHandler("web+neilatools", url);
             // register service worker
             window.UWAWorker = navigator.serviceWorker.register("/service-worker.js");
             window.UWAWorker.then((registration) => logger.log(`Service worker for UWA register success:`, registration)).catch((reason) => logger.error(`Service worker for UWA register fail: ${reason}`));
