540767f5898eb4a3e72ec08654b9fb0e3fe2339c
Neila <neila@neila.ga>
2023-06-24 15:22:36 +0800

    拆分HeadBar.tsx

---
+++ src/components/headBar/SearchIconWrapper.ts
@@ -0,0 +1,13 @@
+import {
+    styled
+} from "@mui/material/styles";
+export const SearchIconWrapper = styled('div')(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}));
+export default SearchIconWrapper;
---
+++ src/components/headBar/StyledInputBase.ts
@@ -0,0 +1,23 @@
+import {
+    InputBase
+} from "@mui/material";
+import {
+    styled
+} from "@mui/material/styles";
+export const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+export default StyledInputBase;
