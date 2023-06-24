596d455e081a67a4486653660a444b94c9737513
Neila <neila@neila.ga>
2023-06-24 10:46:49 +0800

    :package:ServiceWorker

--- src/components/service-worker.js
+++ src/components/service-worker.js
@@ -1,4 +1,8 @@
-import pack from "../package.json";
+import {
+    version,
+    devVersion,
+    dev
+} from "../../package.json";
 /* const toolsTo = [
     "audiotools",
     "countletter",
@@ -10,20 +14,17 @@ import pack from "../package.json";
     "keycode",
     "readnumber",
 ]; */
-export const
-    { version, devVersion, dev } = pack,
-    Cache = `NeilaTools-${version}-${dev == true ? `dev${devVersion}` : "prod"}`, // C
+export const Cache = `NeilaTools-${version}-${dev == true ? `dev${devVersion}` : "prod"}`, // C
     log = text => console.log(`%cServiceWorker`, `background: #52c41a;border-radius: 0.5em;color: white;font-weight: bold;padding: 2px 0.5em`, text),
     installStaticFiles = () => caches.open(Cache).then(cache => cache.addAll(installFilesEssential).catch(console.error)), // install static assets
-    clearOldCaches = () => {
-        return caches.keys().then(keylist => {
-            return Promise.all(keylist.filter(key => {
-                return key !== Cache;
-            }).map(key => {
-                log(`已删除缓存“${key}”`);
-                return caches.delete(key);
-            }));
-        });
+    clearOldCaches = async () => {
+        const keylist = await caches.keys();
+        return await Promise.all(keylist.filter(key => {
+            return key !== Cache;
+        }).map(key_1 => {
+            log(`已删除缓存“${key_1}”`);
+            return caches.delete(key_1);
+        }));
     },
     installFilesEssential = [
         '/',
@@ -48,17 +49,19 @@ self.addEventListener('fetch', event => {
     let { requrl } = event.request;
     let url = String(requrl);
     event.respondWith(
-        caches.open(Cache).then(cache => {
-            return cache.match(event.request).then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then(newreq => {
-                    log(`抓取: ${url}`);
-                    if (newreq.ok) cache.put(event.request, newreq.clone());
-                    return newreq;
-                }).catch(console.error);
-            });
+        caches.open(Cache).then(async cache => {
+            const response = await cache.match(event.request);
+            if (response) {
+                return response;
+            }
+            try {
+                const newreq = await fetch(event.request);
+                log(`抓取: ${url}`);
+                if (newreq.ok) cache.put(event.request, newreq.clone());
+                return newreq;
+            } catch (message) {
+                return console.error(message);
+            }
         })
     );
 });
