e43ca6654b3526af8f1018502687361565155007
Neila <neila@neila.ga>
2023-07-28 23:37:39 +0800

    :fire:整合为一个文件

--- src/tools/readnumber.tsx
+++ src/tools/readnumber.tsx
@@ -13,13 +13,20 @@ export const logger = new LpLogger({
     name: "ReadNumber",
     level: "log", // 空字符串时，不显示任何信息
 });
-import getNzh from "./readnumber/nzh";
 export default function ReadNumber(): JSX.Element {
+    const nzh = new Nzh({
+    ch: I18N.get('零一二三四五六七八九'),
+    ch_u: I18N.get('个十百千万亿兆京垓姊穰沟涧正载极'),
+    ch_f: I18N.get('负'),
+    ch_d: I18N.get('点'),
+    m_u: I18N.get('元角分厘'),
+    m_t: I18N.get('人民币'),
+    m_z: I18N.get('正')
+    });
     var [blur, setBlur] = useState<"number" | "string">("number"),
         [string, setString] = useState<string>(""),
         [number, setNumber] = useState<string>("0");
     function proc(p: string) {
-        const nzh = getNzh(I18N);
         switch (blur) {
             case "number":
                 setString(nzh.encode(p));
