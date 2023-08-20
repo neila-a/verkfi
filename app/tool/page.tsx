8c58f0552ea2ec76bb52de6dce7ede0ecaf27755
Neila <neila@neila.ga>
2023-08-20 22:53:48 +0800

    :bug:

--- app/tool/page.tsx
+++ app/tool/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import {
-    useRouter
-} from "next/router";
-import React, { Fragment } from "react";
+    useRouter,
+    useSearchParams
+} from "next/navigation";
+import React from "react";
 import HeadBar from "../components/headBar/HeadBar";
 import {
     getTools
 } from "../tools/info";
 import {
-    Box, Toolbar
+    Box,
+    Toolbar
 } from "@mui/material";
 import I18N from "react-intl-universal";
 import {
@@ -25,29 +27,28 @@ export default function ToolFinder(): JSX.Element {
         only = false,
         toolID: string,
         Tool: tJSXE,
-	toolsInfo = getTools(I18N);
+        toolsInfo = getTools(I18N);
     const finder = (id: string) => {
         logger.info(`toolID为${id}`);
         toolsInfo.forEach(si => {
-            if (((tools[si.to] as tJSXE) != undefined) && (si.to == id) && si.goto == undefined) {
+            if (((tools[si.to] as tJSXE) != undefined) && (si.to == id) && !si.isGoto) {
                 Tool = tools[si.to];
                 logger.info("<Tool />为", Tool);
             }
         });
-    }
-    if (router.query.tool) {
-        logger.info("query的内容为", router.query);
-        let id = router.query.tool as string;
+    },
+        searchParams = useSearchParams();
+    if (searchParams.has("tool")) {
+        let id = searchParams.get("tool");
         toolID = id;
         finder(id)
     }
-    if (router.query.handle) {
-        logger.info("query的内容为", router.query);
-        let id = (router.query.handle as string).replace(/web\+neilatools:\/\//g, "");
+    if (searchParams.has("handle")) {
+        let id = searchParams.get("handle").replace(/web\+neilatools:\/\//g, "");
         toolID = id;
         finder(id);
     }
-    if (router.query.only == "true") {
+    if (searchParams.has("only")) {
         only = true;
     }
     return (
