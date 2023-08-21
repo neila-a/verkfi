8c3cce96bc733bcd69fa054c66f2209bbb3e3b42
Neila <neila@neila.ga>
2023-08-21 10:03:37 +0800

    Feature request: CL中的其它进制 #115

--- app/tools/countletter/tool.tsx
+++ app/tools/countletter/tool.tsx
@@ -1,6 +1,6 @@
 import I18N from 'react-intl-universal';
 import {
-    Button
+    Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup
 } from "@mui/material";
 import LpLogger from "lp-logger";
 import {
@@ -15,30 +15,51 @@ var logger = new LpLogger({
     level: "log", // 空字符串时，不显示任何信息
 });
 import table from "./table";
+type numberType = 2 | 8 | 10 | 16;
 function CountLetter(): JSX.Element {
     var [enterDialogOpen, setEnterDialogOpen] = useState<boolean>(false),
         [alertDialogOpen, setAlertDialogOpen] = useState<boolean>(false),
-        [out, setOut] = useState<string>("");
+        [out, setOut] = useState<string>(""),
+        [numberType, setNumberType] = useState<numberType>(10);
     return (
-        <div className={style["main"]}>
+        <>
+            <div className={style["main"]}>
+                <Button onClick={() => {
+                    logger.log("已弹出输入框。");
+                    setEnterDialogOpen(true);
+                }} variant="contained" className={style["button"]}>{I18N.get('输入')}</Button>
+                {enterDialogOpen ? <InputDialog context={I18N.get('输入你要转换的字符（串）')} onDone={context => {
+                    table.forEach(single => {
+                        context = context.replace(new RegExp(single, "g"), (table.indexOf(single.toUpperCase()) + 1).toString(numberType).toUpperCase());
+                    })
+                    setEnterDialogOpen(false);
+                    setOut(context);
+                    setAlertDialogOpen(true);
+                    logger.log(`已处理完毕，结果为${context}。`);
+                }} title={I18N.get('输入字符')} label={I18N.get('在这里输入')} /> : <Fragment /> /* 输入对话框容器 */}
+                {alertDialogOpen ? <AlertDialog title={I18N.get('输出')} description={out} onDone={() => {
+                    setAlertDialogOpen(false);
+                }} /> : <Fragment /> /* 输出对话框容器 */}
+            </div>
             <br />
-            <Button onClick={() => {
-                logger.log("已弹出输入框。");
-                setEnterDialogOpen(true);
-            }} variant="contained" className={style["button"]}>{I18N.get('输入')}</Button>
-            {enterDialogOpen ? <InputDialog context={I18N.get('输入你要转换的字符（串）')} onDone={context => {
-                table.forEach(single => {
-                    context = context.replace(new RegExp(single[0], "g"), single[1]);
-                })
-                setEnterDialogOpen(false);
-                setOut(context);
-                setAlertDialogOpen(true);
-                logger.log(`已处理完毕，结果为${context}。`);
-            }} title={I18N.get('输入字符')} label={I18N.get('在这里输入')} /> : <Fragment /> /* 输入对话框容器 */}
-            {alertDialogOpen ? <AlertDialog title={I18N.get('输出')} description={out} onDone={() => {
-                setAlertDialogOpen(false);
-            }} /> : <Fragment /> /* 输出对话框容器 */}
-        </div>
+            <FormControl>
+                <FormLabel id="radio-buttons-group-label">数字进制</FormLabel>
+                <RadioGroup
+                    aria-labelledby="radio-buttons-group-label"
+                    defaultValue="10"
+                    value={numberType}
+                    onChange={event => {
+                        setNumberType(event.target.value as unknown as numberType);
+                    }}
+                    name="radio-buttons-group"
+                >
+                    <FormControlLabel value="16" control={<Radio />} label="十六进制" />
+                    <FormControlLabel value="10" control={<Radio />} label="十进制" />
+                    <FormControlLabel value="8" control={<Radio />} label="八进制" />
+                    <FormControlLabel value="2" control={<Radio />} label="二进制" />
+                </RadioGroup>
+            </FormControl>
+        </>
     );
 };
 export default CountLetter;
--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
-const esbuildLoader = require('esbuild-loader');
 const nextConfig = {
-	webpack: (config, options) => {
-    // 添加 esbuildLoader 配置
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      loader: esbuildLoader.Loader,
-      options: {
-        loader: 'jsx', // 设定处理的文件类型
-        target: 'es2015' // 设定生成的代码规范类型
-      }
-    });
-    return config;
-  },
 	reactStrictMode: true,
 	swcMinify: true,
 	typescript: {
