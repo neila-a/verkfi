831ac07fc6424e26a588e93808779fba89315956
Neila <neila@neila.ga>
2023-09-09 15:09:35 +0800

    🏷️ next.config.js改为TypeScript并自动编译

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type {
+	NextConfig
+} from "next";
+const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	typescript: {
