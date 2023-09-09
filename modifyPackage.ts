f9da5ef1a96d90d10cf097aec76132c862615e0f
Neila <neila@neila.ga>
2023-09-09 19:37:10 +0800

    🐛 自动检测remote

--- modifyPackage.ts
+++ modifyPackage.ts
@@ -2,11 +2,13 @@ import * as fs from "node:fs";
 import pack from "./package.json";
 import ChildProcess from "node:child_process";
 import Logger from "lp-logger";
-ChildProcess.execSync("git pull origin main");
 const logger = new Logger({
     name: "ModifyPackage",
     level: "log"
-});
+}),
+    remote1 = ChildProcess.execFileSync("git remote").toString().replace(/\n/g, "");
+logger.log(`检测到第一顺位remote为${remote1}`)
+ChildProcess.execSync(`git pull ${remote1} main`);
 let commits = Number(ChildProcess.execSync("git log --oneline | wc -l").toString().replace(/\n/g, "")),
     oldPackage = <typeof pack>JSON.parse(fs.readFileSync("package.json").toString());
 commits++;
