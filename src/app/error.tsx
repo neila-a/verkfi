9b5c1fd52f21e8f831a8e7f1212202413f7869b7
Neila <neila@neila.ga>
2023-09-10 16:18:24 +0800

    🐛 error无需设置外层

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,33 +3,27 @@ import {
     Button,
     Typography
 } from "@mui/material";
-import Layout from "./layout";
 import Loading from "./loading";
 import I18N from "react-intl-universal";
-export default function GlobalError({
-    error,
-    reset,
-}: {
-    error: Error
-    reset: () => void
+export default function GlobalError(props: {
+    error: Error;
+    reset: () => void;
 }) {
     return (
-        <Layout>
-            <Loading>
-                <Typography sx={{
-                    textAlign: "center"
-                }}>
-                    {I18N.get("发生了一些错误")}
-                    <br />
-                    {String(error)}
-                </Typography>
+        <Loading>
+            <Typography sx={{
+                textAlign: "center"
+            }}>
+                {I18N.get("发生了一些错误")}
                 <br />
-                <Button variant="contained" onClick={event => {
-                    reset();
-                }}>
-                    {I18N.get("重试")}
-                </Button>
-            </Loading>
-        </Layout>
+                {String(props.error)}
+            </Typography>
+            <br />
+            <Button variant="contained" onClick={event => {
+                props.reset();
+            }}>
+                {I18N.get("重试")}
+            </Button>
+        </Loading>
     );
 }
