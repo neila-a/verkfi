778c93e5b89a436d6793686220e0a82e5fa9085e
Neila <neilaspace@outlook.com>
2023-10-15 10:07:58 +0800

    💄 🐛 Bug report: 翻转中的间隔太小 #125

--- src/app/tools/reversal/page.tsx
+++ src/app/tools/reversal/page.tsx
@@ -25,6 +25,7 @@ var logger = new LpLogger({
     level: "log", // 空字符串时，不显示任何信息
 });
 import destroyer from "../../components/destroyer";
+import { Box } from '@mui/system';
 function Reversal(): JSX.Element {
     var [wordList, setWordList] = useState<[string, number][]>([]),
         [words, setWords] = useState<string>(""),
@@ -34,58 +35,65 @@ function Reversal(): JSX.Element {
         [showSplitDialog, setShowSplitDialog] = useState<boolean>(false),
         [showCopyDoneDialog, setShowCopyDoneDialog] = useState<boolean>(false);
     return (
-        <>
-            <Alert severity="info">{I18N.get('以空格分隔，按回车添加。')}</Alert>
-            <TextField fullWidth label={I18N.get('输入框')} id="input" onKeyDown={event => {
-                if (event.key == "Enter") {
-                    setWordList(current => [...current, [
-                        words,
-                        Math.random() * 1000000000000000
-                    ]]);
-                    setWords("");
-                    logger.log("已保存至列表。");
+        <Box>
+            <Box sx={{
+                "> *": {
+                    marginBottom: 1
                 }
-            }} onChange={event => {
-                setWords(event.target.value);
-            }} value={words} />
-            {wordList.map(wordArray => {
-                return (
-                    <Chip key={wordArray[1]} label={wordArray[0]} onDelete={() => {
-                        setWordList(oldWordList => destroyer(oldWordList, wordArray));
-                    }} deleteIcon={<Close />} />
-                );
-            })}
-            <br />
-            <ButtonGroup>
-                <Button variant="contained" onClick={() => {
-                    var stageOutput: [string, number][] = wordList;
-                    stageOutput.sort(() => {
-                        return Math.random() - 0.5;
-                    });
-                    setOutput(stageOutput.map(wordArray => {
-                        return wordArray[0];
-                    }).join(""));
-                    logger.log("已处理。");
-                }}>{I18N.get('处理')}</Button>
-                <Button variant="outlined" onClick={() => {
-                    setWordList([]);
-                    setWords("");
-                    setOutput("");
-                    logger.log("已重置。");
-                }}>{I18N.get('重来')}</Button>
-                <Button variant="outlined" onClick={() => {
-                    setShowSplitDialog(true);
-                    logger.log("已打开输入对话框。");
-                }}>{I18N.get('拆分')}</Button>
-                <Button variant="outlined" onClick={() => {
-                    navigator.clipboard.writeText(output).then(() => {
-                        setShowCopyDoneDialog(true);
-                    }).catch(error => {
-                        setCopyError(`复制结果时出现错误，请报告给开发人员：${error}`);
-                        setShowCopyErrorDialog(true);
-                    });
-                }}>{I18N.get('复制')}</Button>
-            </ButtonGroup>
+            }}>
+                <Alert severity="info">
+                    {I18N.get('以空格分隔，按回车添加。')}
+                </Alert>
+                <TextField fullWidth label={I18N.get('输入框')} id="input" onKeyDown={event => {
+                    if (event.key == "Enter") {
+                        setWordList(current => [...current, [
+                            words,
+                            Math.random() * 1000000000000000
+                        ]]);
+                        setWords("");
+                        logger.log("已保存至列表。");
+                    }
+                }} onChange={event => {
+                    setWords(event.target.value);
+                }} value={words} />
+                {wordList.map(wordArray => {
+                    return (
+                        <Chip key={wordArray[1]} label={wordArray[0]} onDelete={() => {
+                            setWordList(oldWordList => destroyer(oldWordList, wordArray));
+                        }} deleteIcon={<Close />} />
+                    );
+                })}
+                <ButtonGroup fullWidth>
+                    <Button variant="contained" onClick={() => {
+                        var stageOutput: [string, number][] = wordList;
+                        stageOutput.sort(() => {
+                            return Math.random() - 0.5;
+                        });
+                        setOutput(stageOutput.map(wordArray => {
+                            return wordArray[0];
+                        }).join(""));
+                        logger.log("已处理。");
+                    }}>{I18N.get('处理')}</Button>
+                    <Button variant="outlined" onClick={() => {
+                        setWordList([]);
+                        setWords("");
+                        setOutput("");
+                        logger.log("已重置。");
+                    }}>{I18N.get('重来')}</Button>
+                    <Button variant="outlined" onClick={() => {
+                        setShowSplitDialog(true);
+                        logger.log("已打开输入对话框。");
+                    }}>{I18N.get('拆分')}</Button>
+                    <Button variant="outlined" onClick={() => {
+                        navigator.clipboard.writeText(output).then(() => {
+                            setShowCopyDoneDialog(true);
+                        }).catch(error => {
+                            setCopyError(`复制结果时出现错误，请报告给开发人员：${error}`);
+                            setShowCopyErrorDialog(true);
+                        });
+                    }}>{I18N.get('复制')}</Button>
+                </ButtonGroup>
+            </Box>
             <Typography variant="body1">{I18N.get('结果：')}{output}</Typography>
             {showCopyErrorDialog && <AlertDialog onDone={() => {
                 setShowCopyErrorDialog(false);
@@ -103,7 +111,7 @@ function Reversal(): JSX.Element {
             <Snackbar open={showCopyDoneDialog} message={I18N.get('已把结果复制至剪贴板。')} onClose={() => {
                 setShowCopyDoneDialog(false);
             }}/>
-        </>
+        </Box>
     );
 };
 export default Reversal;
