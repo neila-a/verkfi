95e230707e1c9683a8e5f167c057f9f85812cf15
Neila <neilaspace@outlook.com>
2024-02-22 14:51:07 +0800

    ⚡️ 使用ServiceWorker返回301来代替handle/page

--- src/app/service-worker.ts
+++ src/app/service-worker.ts
@@ -51,42 +51,52 @@ self.addEventListener('activate', event => event.waitUntil((async () => {
     await clearOldCaches();
     return self.clients.claim();
 })()));
+/**
+ * 判断顺序：handle -> customRoute -> _rsc  -> fetch -> cache
+ * handle：自定义，并且要快
+ * customRoute：自定义
+ * _rsc：拦截_rsc
+ * fetch：正常模式
+ * cache：缓存回退模式
+ */
 self.addEventListener('fetch', event => {
     if (event.request.method !== 'GET') return;
     let requrl = event.request.url;
     let url = String(requrl);
-    let path = new URL(url).pathname.split("/");
+    let urled = new URL(url);
+    let path = urled.pathname.split("/");
     path.shift();
     event.respondWith((async () => {
         const cache = await caches.open(Cache);
         var realReq = event.request.clone();
         var response: Response;
-        if (url.includes("_rsc=")) {
+        const newreq = await fetch(realReq);
+        log(`抓取: ${url}`);
+        if (path[0] === "handle") {
+            const toPath = `/tools/${urled.searchParams.get("handle").replace(/web\+verkfi:\/\//g, "")}`;
+            log(`检测到URL：${url}中含有"handle"，重定向至${toPath}`);
+            const headers = new Headers();
+            headers.append("Location", toPath);
+            response = new Response("", {
+                status: 301,
+                statusText: "Redirected by ServiceWorker because this is a handler page",
+                headers: headers
+            });
+        } else if (path[0] === "extendedfiles") {
+            response = new Response(new Blob([(await db.extendedTools.toArray()).filter(item => item.to === path[1])[0].files.filter(item => item[0] === path[2])[0][1]]));
+        } else if (url.includes("_rsc=")) {
             log(`检测到URL：${url} 中含有searchParam“rsc”，已返回无rsc版本`);
             response = await cache.match(realReq, {
                 ignoreSearch: true
             });
-        } else if (path[0] === "extendedfiles") {
-            response = new Response(new Blob([(await db.extendedTools.toArray()).filter(item => item.to === path[1])[0].files.filter(item => item[0] === path[2])[0][1]]));
+        } else if (newreq.ok && newreq.status === 200) {
+            cache.put(realReq, newreq.clone());
+            response = newreq;
         } else {
             response = await cache.match(realReq);
         }
         if (response) {
             return response;
-        }
-        if (url.includes("handle")) {
-            log(`检测到URL：${url}中含有"handle"，不存入缓存`);
-            const newreq = await fetch(realReq);
-            return newreq;
-        }
-        try {
-            const newreq = await fetch(realReq);
-            log(`抓取: ${url}`);
-            if (newreq.ok) cache.put(realReq, newreq.clone());
-            return newreq;
-        } catch (message) {
-            console.error(message);
-            return message;
         }
     })());
 });
