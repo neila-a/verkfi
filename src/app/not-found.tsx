b392d33931f8bb36b414d9dece9ce062ed573310
Neila <neilaspace@outlook.com>
2024-04-05 22:19:41 +0800

    🥅 添加NotFoundError类型

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,11 +5,17 @@ import {
 import {
     default as ErrorElement
 } from "./error";
+class NotFoundError extends Error {
+    constructor(message = '404') {
+        super(message);
+        this.name = 'NotFoundError';
+    }
+}
 export default function NotFound() {
     const router = useRouter();
     return (
         <ErrorElement reset={() => {
             router.push("/");
-        }} error={new Error("HTTP Error 404")} />
+        }} error={new NotFoundError()} />
     );
 }
