211e47aae08947ae7bcd3052f27b136333f4976d
Neila <neilaspace@outlook.com>
2024-05-04 21:28:11 +0800

    🏷️ 修正desktopAdder的类型

--- packages/core/src/app/layout/registers/desktopAdder.ts
+++ packages/core/src/app/layout/registers/desktopAdder.ts
@@ -7,12 +7,11 @@ import {
 } from "../layoutClient";
 const desktopAdder = () => {
     const defaultPrompt = {
-        prompt() {
+        async prompt() {
             logger.error("无法安装");
         }
     };
-    //@ts-ignore 不是，你不能安装PWA还强行安装，指定没好果汁吃奥
-    let deferredPrompt: BeforeInstallPromptEvent = defaultPrompt;
+    let deferredPrompt: Partial<BeforeInstallPromptEvent> & Pick<BeforeInstallPromptEvent, "prompt"> = defaultPrompt;
     // 监听beforeinstallprompt事件，该事件在网站满足PWA安装条件时触发，保存安装事件
     window.addEventListener("beforeinstallprompt", (event: BeforeInstallPromptEvent) => {
         event.preventDefault();
@@ -20,7 +19,6 @@ const desktopAdder = () => {
     });
     // 监听appinstalled事件，该事件在用户同意安装后触发，清空安装事件
     window.addEventListener("appinstalled", () => {
-        //@ts-ignore 不是，你安装完了还想安装，指定没好果汁吃奥
         deferredPrompt = defaultPrompt;
     });
     // 手动触发PWA安装
