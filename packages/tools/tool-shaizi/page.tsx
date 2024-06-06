243fa323828ee5ef9ba9aeb15f183eff9d32a3f5
Neila <neilaspace@outlook.com>
2024-06-06 22:31:16 +0800

    🔥 删除骰子全屏模式

--- packages/tools/tool-shaizi/page.tsx
+++ packages/tools/tool-shaizi/page.tsx
@@ -14,10 +14,8 @@ import {
 import {
     ShaiZiCanvas
 } from "./shaiziCanvas";
-const FullScreenDialog = dynamic(() => import("@verkfi/shared/dialog/FullScreen"));
 function ShaiZi(): JSX.Element {
-    const [useDialogShow, setUseDialogShow] = useState<boolean>(false),
-        [cishu, setCishu] = useState<number>(10);
+    const [cishu, setCishu] = useState<number>(10);
     return (
         <>
             <Stack spacing={2} component="section">
@@ -25,15 +23,7 @@ function ShaiZi(): JSX.Element {
                 <TextField label={get("shaizi.掷色子的次数")} variant="outlined" value={cishu} type="number" onChange={event => {
                     setCishu(Number(event.target.value));
                 }} />
-                <Button variant="contained" onClick={event => {
-                    setUseDialogShow(true);
-                }}>
-                    {get("shaizi.全屏")}
-                </Button>
             </Stack>
-            <FullScreenDialog open={useDialogShow} title={get("shaizi.掷色子（全屏模式）")} onDone={() => {
-                setUseDialogShow(false);
-            }} context={<ShaiZiCanvas cishu={cishu} />} />
         </>
     );
 }
