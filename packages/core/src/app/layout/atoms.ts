768224407080616694f5fe4c8feb634258af2dd8
Neila <neilaspace@outlook.com>
2024-06-14 20:06:24 +0800

    ✨ clientsAtom利用atomWithBroadcast同步

--- packages/core/src/app/layout/atoms.ts
+++ packages/core/src/app/layout/atoms.ts
@@ -1,9 +1,19 @@
 "use client";
+import atomWithBroadcast from "@verkfi/shared/reader/atomWithBroadcast";
+import atomWithInitialValue, {
+    valueAtomReturn
+} from "@verkfi/shared/reader/atomWithInitialValue";
 import {
-    atom
-} from "jotai";
+    emptySymbol
+} from "@verkfi/shared/reader/atomWithStorage";
 interface clientBase {
     id: string;
     url: string;
 }
-export const clientsAtom = atom<clientBase[]>([]);
+export const clientsAtom = atomWithInitialValue((valueAtom: valueAtomReturn<clientBase[]>) => atomWithBroadcast(get => {
+    const got = get(valueAtom);
+    if (got === emptySymbol) {
+        return [];
+    }
+    return got;
+}, (get, set, update: clientBase[]) => set(valueAtom, update), "clients"))[0];
