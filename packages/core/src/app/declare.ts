0117d72709542ccad7b160196f804ae6f53b054d
Neila <neilaspace@outlook.com>
2024-06-23 17:18:11 +0800

    ⬆️

--- packages/core/src/app/declare.ts
+++ packages/core/src/app/declare.ts
@@ -1,8 +1,3 @@
-import {
-    Dispatch,
-    SetStateAction
-} from "react";
-
 /**
  * The BeforeInstallPromptEvent is fired at the Window.onbeforeinstallprompt handler
  * before a user is prompted to "install" a web site to a home screen on mobile.
