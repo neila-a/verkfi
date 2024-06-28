6f09bec00d084b24c4ce0678d141d9f36299fa2a
Neila <neilaspace@outlook.com>
2024-06-28 23:31:54 +0800

    🎨 prebuilder中childprocess接到的数据输出后不添加换行

--- packages/core/prebuilder/buildNext.ts
+++ packages/core/prebuilder/buildNext.ts
@@ -5,13 +5,13 @@ export default function buildNext() {
     console.log("开始编译Verkfi".brightCyan);
     const next = getNextProcess();
     next.stdout.on("data", chunk => {
-        console.log(String(chunk).brightBlue);
+        process.stdout.write(String(chunk).brightBlue);
     }).on("close", () => {
         console.log("编译Verkfi成功完成".brightCyan);
         console.timeEnd("编译Verkfi耗时".brightCyan);
     });
     next.stderr.on("data", chunk => {
-        console.error(String(chunk).brightRed);
+        process.stderr.write(String(chunk).brightRed);
     }).on("close", () => {
         console.error("编译Verkfi以错误退出".brightRed);
     });
